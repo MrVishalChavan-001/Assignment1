6f6406d3f344e5ee2d95be3254c9f56d95cdc8d7
MrVishalChavan-001 <vishalpchavan37@gmail.com>
2023-09-08 12:43:00 +0530

    cmt

--- AssignmentGitHub/src/com/jsp/assignment1/Assignment2.java
+++ AssignmentGitHub/src/com/jsp/assignment1/Assignment2.java
@@ -2,7 +2,7 @@ package com.jsp.assignment1;
 
 public class Assignment2 {
 
-	public static void main(String[] args) {
+	public static void test() {
 		// TODO Auto-generated method stub
 System.out.println("Hii");
 	}
